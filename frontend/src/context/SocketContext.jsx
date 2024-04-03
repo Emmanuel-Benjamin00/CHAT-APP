42a974d8bece3572b58b0f32f9c5486fecb38f0f
Emmanuel Benjamin D <emmanuel26112000@gmail.com>
2024-04-03 21:59:06 +0530

    socket context changed

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-web-app-jjzt.onrender.com", {
+			const socket = io("https://chat-app-si2s.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
